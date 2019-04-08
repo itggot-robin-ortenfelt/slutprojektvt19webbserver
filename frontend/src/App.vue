4915bed938017627986112d13eeef392b4d11cf3
itggot-robin-ortenfelt <38528650+itggot-robin-ortenfelt@users.noreply.github.com>
2019-04-08 10:37:33 +0200

    Copied everything

---
+++ frontend/src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    
+    <main>
+      <section class="trending">
+        <header>
+      <section>
+        <div class="input">
+          <input type="text">
+        </div>
+        <aside class="cart">
+          <!-- <img src="@/assets/cart.svg" @click="views.cart = !views.cart" />
+          <div class="badge">{{ cart.length }}</div>
+          <cart :cart="cart" v-if="views.cart"></cart>-->
+        </aside>
+        <aside class="profile">
+          <img src="@/assets/profile.png" alt>
+        </aside>
+      </section>
+      <nav>
+        <ul>
+          <li>
+            <a href>New</a>
+          </li>
+          <li>
+            <a href>Brands</a>
+          </li>
+          <li>
+            <a href>Shop</a>
+          </li>
+          <li>
+            <a href>Settings</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+        <img src alt="GUL">
+      </section>
+      <section class="tops">
+        <div>
+          <h2>TOPS</h2>
+        </div>
+        <section class="kategories">
+          <a href="#">
+            <img src="" alt="BILD">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD1">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD2">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD3">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD4">
+          </a>
+         
+        </section>
+      </section>
+      <section class="shoes">
+        <div>
+          <h2>SHOES</h2>
+        </div>
+         <section class="kategories">
+          <a href="#">
+            <img src="" alt="BILD">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD1">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD2">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD3">
+          </a>
+          <a href="#">
+            <img src="" alt="BILD4">
+          </a>
+        </section>
+      </section>
+    </main>
+    <section class="footer">
+        FOTER
+      </section>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+// import products from './assets/products';
+// import Item from '@/components/items';
+
+// export default {
+//   name: "app",
+//   components: {
+//     Item
+//   },
+//   data(){
+//     return{
+//       products: products
+//     }
+//   }
+// };
+</script>
+
+<style lang="scss">
+@import "scss/main.scss";
+</style>
---
+++ frontend/src/components/items.vue
@@ -0,0 +1,30 @@
+<template>
+  <article>
+    <div class="img" v-on:click="showProduct(item)">
+      <img v-bind:src="'@/assets/products/' + item.imgFile" :alt="item.color">
+    </div>
+    <footer>
+      <h2 class="title">{{ item.name }}</h2>
+      <h3 class="price">{{ item.price }};-</h3>
+    </footer>
+  </article>
+</template>
+
+<script>
+// export default {
+//     name: "Item",
+//     props: ['item'],
+//     methods:
+//     {
+//         showProduct(item) {
+//             // lägg till item som active product
+//             this.$parent.activeProduct = item;
+//             // toggla view.shoeProdukt
+//             this.$parent.views.singleProduct = true;
+//         }
+//     },
+// };
+</script>
+
+<style>
+</style>
